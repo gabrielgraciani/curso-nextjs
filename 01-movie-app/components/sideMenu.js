bc09b41ee608d9560b8ea0fbd33ae1744e7d740a
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-10 21:15:22 -0300

    aula 14 - projeto 01-movie-app - useState

--- 01-movie-app/components/sideMenu.js
+++ 01-movie-app/components/sideMenu.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const SideMenu = () => (
-	<div>
-		<h1 className="my-4">Shop Name</h1>
-		<div className="list-group">
-			<a href="#" className="list-group-item">Category 1</a>
-			<a href="#" className="list-group-item">Category 2</a>
-			<a href="#" className="list-group-item">Category 3</a>
+const SideMenu = () => {
+	const [count, setCount] = useState(0);
+
+	const increment = () => {
+		const newCount = count + 1;
+		setCount(newCount);
+	};
+
+	const decrement = () => {
+		const newCount = count - 1;
+		setCount(newCount);
+	};
+	return(
+		<div>
+			<h1 className="my-4">Shop Name</h1>
+			<div className="list-group">
+				<a href="#" className="list-group-item">Category 1</a>
+				<a href="#" className="list-group-item">Category 2</a>
+				<a href="#" className="list-group-item">Category 3</a>
+			</div>
+
+			<div>
+				<button className="btn btn-primary" onClick={increment}>Increment Number</button>
+				<button className="btn btn-primary" onClick={decrement}>Decrement Number</button>
+
+				<h1>{count}</h1>
+			</div>
 		</div>
-	</div>
-);
+	)
+};
 
 export default SideMenu;
