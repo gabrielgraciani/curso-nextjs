628997cbcd0843da607939bb97f8fd0eebad55a5
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-11 16:27:35 -0300

    aula 35 - projeto 01-movie-app - modal implementation + close modal

---
+++ 01-movie-app/components/modal.js
@@ -0,0 +1,45 @@
+const Modal = () => {
+
+	let closeButton = null;
+	const submitModal = () => {
+		alert('submit');
+		closeButton.click();
+	};
+
+	return(
+		<>
+			<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+				Create Movie
+			</button>
+
+			<div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog" role="document">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">Create Movie</h5>
+							<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div className="modal-body">
+							...
+						</div>
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-secondary"
+								data-dismiss="modal"
+								ref={element => closeButton = element}>Close</button>
+							<button
+								type="button"
+								className="btn btn-primary"
+								onClick={submitModal}>Save changes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+};
+
+export default Modal;
