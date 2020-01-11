0f95462a0e003ecb807478eab0d9876c5513a34f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-11 13:41:32 -0300

    aula 28 - projeto 01-movie-app - detail page of movie

--- 01-movie-app/components/movieList.js
+++ 01-movie-app/components/movieList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Link from 'next/link';
 
 class MovieList extends Component{
 
@@ -14,10 +15,14 @@ class MovieList extends Component{
 		return movies.map((movie) => (
 			<div className="col-lg-4 col-md-6 mb-4" key={movie.id}>
 				<div className="card h-100">
-					<a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
+					<Link href={`/movies/${movie.id}`}>
+					<a><img className="card-img-top" src={movie.image} alt=""/></a>
+					</Link>
 					<div className="card-body">
 						<h4 className="card-title">
-							<a href="#">{movie.name}</a>
+							<Link href={`/movies/${movie.id}`}>
+							<a>{movie.name}</a>
+							</Link>
 						</h4>
 						<p className="card-text">{this.shorten(movie.description, 100)}</p>
 					</div>
