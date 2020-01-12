694f5113901e815c33ae9636eba65df4a8c3d06d
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-12 20:45:37 -0300

    Começo desenvolvimento portfolio atualizado

--- 02-portfolio/server.js
+++ 02-portfolio/server.js
@@ -14,9 +14,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.use(handle).listen(3000, (err) => {
+  server.use(handle).listen(3001, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:3001')
   })
 })
 .catch((ex) => {
